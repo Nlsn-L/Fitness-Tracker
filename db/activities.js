304f856a025444528e532d41ebee30561d6893e7
Nelson <n911luna@gmail.com>
2022-11-08 13:07:06 -0600

    fiex activities functions, all test up and running

--- db/activities.js
+++ db/activities.js
@@ -1,12 +1,13 @@
-const client = require("./client")
+const client = require("./client");
+const { getUser } = require("./users");
 
 // database functions
-async function getAllActivities() {
+
+async function getAllActivities(){
 try {
   const {rows} = await client.query(`
   SELECT *
-  FROM activities
-  RETURNING *;
+  FROM activities;
   `);
 
   return rows;
@@ -22,8 +23,8 @@ async function getActivityById(id) {
     const {rows: [activity] } = await client.query(
       `SELECT *
        FROM activities
-       WHERE id = ${id}
-    `);
+       WHERE id = $1
+    `,[id]);
 
     return activity;
 
@@ -34,24 +35,38 @@ async function getActivityById(id) {
 }
 
 async function getActivityByName(name) {
+  
+  try {
+
+    const {rows: [activity]} = await client.query(`
+      SELECT *
+      FROM activities    
+      WHERE name=$1
+    `,[name])
+
+    return activity
+  } catch (error) {
+    console.error("Error getting activity by name!")
+    throw error;
+  }
 
 }
 
 // select and return an array of all activities
-async function attachActivitiesToRoutines(routines) {
-}
+// async function attachActivitiesToRoutines(routines) {
+// }
 
 // return the new activity
 async function createActivity({ name, description }) {
   try {
-    const {rows} = await client.query(`
-    INSERT INTO (name,description)
+    const {rows: [activity]} = await client.query(`
+    INSERT INTO activities(name,description)
     VALUES ($1,$2)
     ON CONFLICT (name) DO NOTHING
     RETURNING *;
-    `)
-
-    return rows
+    `,[name,description])
+   
+    return activity
   } catch (error) {
     console.error("Error creating and activity!")
     throw error;
@@ -65,22 +80,21 @@ async function createActivity({ name, description }) {
 // return the updated activity
 async function updateActivity({ id, ...fields }) {
     const setString = Object.keys(fields).map(
-      (key,index) => {
-        `"${key}" = $${index + 1}`
+      (values,index) => {
+        return `"${values}" = $${index + 1}`
       }
-    ).join(', ')
+    ).join(", ")
 
       if (setString.length === 0){
         return;
       }
     try{
-      const result = await client.query(`
-      UPDATE users
+      const {rows: [result]} = await client.query(`
+      UPDATE activities
       SET ${setString}   
       WHERE id=${id}
       RETURNING *;   
       `,Object.values(fields))
-
       return result
     }catch (error) {
       console.error("Error updating activity!")
@@ -89,34 +103,35 @@ async function updateActivity({ id, ...fields }) {
 
 }
 
-// async function attachActivitiesToRoutines(routines) {
-//   // no side effects
-//   const routinesToReturn = [...routines];
-//   const binds = routines.map((_, index) => `$${index + 1}`).join(', ');
-//   const routineIds = routines.map(routine => routine.id);
-//   if (!routineIds?.length) return [];
+async function attachActivitiesToRoutines(routines) {
+  // no side effects
+  const routinesToReturn = [...routines];
+  const binds = routines.map((_, index) => `$${index + 1}`).join(', ');
+  const routineIds = routines.map(routine => routine.id);
+  if (!routineIds?.length) return [];
   
-//   try {
-//     // get the activities, JOIN with routine_activities (so we can get a routineId), and only those that have those routine ids on the routine_activities join
-//     const { rows: activities } = await client.query(`
-//       SELECT activities.*, routine_activities.duration, routine_activities.count, routine_activities.id AS "routineActivityId", routine_activities."routineId"
-//       FROM activities 
-//       JOIN routine_activities ON routine_activities."activityId" = activities.id
-//       WHERE routine_activities."routineId" IN (${ binds });
-//     `, routineIds);
+  try {
+    // get the activities, JOIN with routine_activities (so we can get a routineId), and only those that have those routine ids on the routine_activities join
+    const { rows: activities } = await client.query(`
+      SELECT activities.*, routine_activities.duration, routine_activities.count, routine_activities.id AS "routineActivityId", routine_activities."routineId"
+      FROM activities 
+      JOIN routine_activities ON routine_activities."activityId" = activities.id
+      WHERE routine_activities."routineId" IN (${ binds });
+    `, routineIds);
 
-//     // loop over the routines
-//     for(const routine of routinesToReturn) {
-//       // filter the activities to only include those that have this routineId
-//       const activitiesToAdd = activities.filter(activity => activity.routineId === routine.id);
-//       // attach the activities to each single routine
-//       routine.activities = activitiesToAdd;
-//     }
-//     return routinesToReturn;
-//   } catch (error) {
-//     throw error;
-//   }
-// }
+    // loop over the routines
+    for(const routine of routinesToReturn) {
+      // filter the activities to only include those that have this routineId
+      const activitiesToAdd = activities.filter(activity => activity.routineId === routine.id);
+      // attach the activities to each single routine
+      routine.activities = activitiesToAdd;
+    }
+    return routinesToReturn;
+  } catch (error) {
+    console.error("Error attaching activities to routines!")
+    throw error;
+  }
+}
 
 
 
