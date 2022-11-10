3c670045393d1dbf0318bccb848224584d1617c5
Nelson <n911luna@gmail.com>
2022-11-10 14:33:05 -0600

    added files for server to work, issues with client

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
-
+const cors = require("cors")
 // Setup your Middleware and API Router here
+app.use(cors())
+const morgan = require("morgan");
+app.use(morgan("dev"));
+
+app.use(express.json());
+const { client } = require('./db')
+client.connect();
+
+const apiRouter = require("./api")
+app.use('/api',apiRouter)
 
 module.exports = app;
