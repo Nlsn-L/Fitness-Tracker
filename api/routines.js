b246d6468d91e87ecfa92801cceb02836b281639
Brevan <brev.bdawg@gmail.com>
2022-11-14 15:51:49 -0600

    1/10 routines tests passing, 3/10 attempted so far

--- api/routines.js
+++ api/routines.js
@@ -1,10 +1,30 @@
-const express = require('express');
+const express = require("express");
+const { getAllPublicRoutines, createRoutine } = require("../db");
+const { requireUser } = require("./utils");
 const router = express.Router();
 
 // GET /api/routines
+router.get("/", async (req, res, next) => {
+  try {
+    const allPublicRoutines = await getAllPublicRoutines();
 
+    res.send(allPublicRoutines);
+  } catch (error) {
+    next();
+  }
+});
 // POST /api/routines
+router.post("/", requireUser, async (req, res, next) => {
+  const { name, goal, isPublic, creatorId } = req.body;
 
+  try {
+    const newRoutine = await createRoutine({name, goal, isPublic, creatorId});
+
+    res.send(newRoutine);
+  } catch (error) {
+    throw error;
+  }
+});
 // PATCH /api/routines/:routineId
 
 // DELETE /api/routines/:routineId
